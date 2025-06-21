4a64e29b6e3c32cf7383f525d4288d6edd54b0f9
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-06-21 12:17:47 -0600

    Changes in UI

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -9,7 +9,7 @@ export function Card({
 }): JSX.Element {
   return (
     <div className="border p-6 bg-white rounded-xl bg-[#ededed]">
-      <h1 className="text-2xl border-b pb-2">{title}</h1>
+      <h1 className="text-2xl border-b pb-4">{title}</h1>
       <p>{children}</p>
     </div>
   );
